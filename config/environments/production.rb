a6075afc12f57dbea98351d208c9c2abb12aa823
Don <dshine@gofree.indigo.ie>
2016-08-31 14:20:03 +0200

    production.rb changes for css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
